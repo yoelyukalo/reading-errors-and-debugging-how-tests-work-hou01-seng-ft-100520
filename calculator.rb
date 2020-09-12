5de477e0949d8a79d0bfe82f658f5996b79aae2a
Yoel Yukalo <yoelyukalo@gmail.com>
2020-09-12 19:21:16 +0000

    Done.

--- calculator.rb
+++ calculator.rb
@@ -1,1 +1,11 @@
-# Add your variables here
+first_number = 777
+
+second_number = 1
+
+sum = first_number + second_number
+
+difference = first_number - second_number
+
+product = first_number * second_number
+
+quotient = first_number / second_number
